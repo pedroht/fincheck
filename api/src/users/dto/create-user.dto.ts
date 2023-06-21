e4c22194ce3cccfa2c536a2f5d70f96a9b392935
Pedro Henrique <torres.pedrohen@gmail.com>
2023-06-21 19:54:50 -0300

    feat: create user dto + validation

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({
+    message: 'Campo nome é obrigatório',
+  })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({
+    message: 'Campo email é obrigatório',
+  })
+  @IsEmail({}, { message: 'Necessário informar um email válido' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({
+    message: 'Campo senha é obrigatório',
+  })
+  @MinLength(8, {
+    message: 'Campo senha deve ter no mínimo 8 caracteres',
+  })
+  password: string;
+}
