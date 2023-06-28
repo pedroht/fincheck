d317c95bc1babcab4889fae8640490d27a4f4a3f
Pedro Henrique <torres.pedrohen@gmail.com>
2023-06-27 23:04:44 -0300

    feat: add module bankAccounts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { DatabaseModule } from './shared/database/database.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
   controllers: [],
   providers: [
     {
