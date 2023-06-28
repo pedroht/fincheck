eaf3c1a6c34cd87c17771ec4eb0f50fd217f5a7e
Pedro Henrique <torres.pedrohen@gmail.com>
2023-06-27 23:27:27 -0300

    feat: implement create bank account service

--- api/src/modules/bank-accounts/bank-accounts.controller.ts
+++ api/src/modules/bank-accounts/bank-accounts.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { BankAccountsService } from './bank-accounts.service';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
   constructor(private readonly bankAccountsService: BankAccountsService) {}
 
   @Post()
-  create(@Body() createBankAccountDto: CreateBankAccountDto) {
-    return this.bankAccountsService.create(createBankAccountDto);
+  create(
+    @ActiveUserId() userId: string,
+    @Body() createBankAccountDto: CreateBankAccountDto,
+  ) {
+    return this.bankAccountsService.create(userId, createBankAccountDto);
   }
 
   @Get()
@@ -23,7 +35,10 @@ export class BankAccountsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBankAccountDto: UpdateBankAccountDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateBankAccountDto: UpdateBankAccountDto,
+  ) {
     return this.bankAccountsService.update(+id, updateBankAccountDto);
   }
 
--- api/src/shared/database/repositories/bank-accounts.repositories.ts
+++ api/src/shared/database/repositories/bank-accounts.repositories.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../prisma.service';
+import { type Prisma } from '@prisma/client';
 
 @Injectable()
 export class BankAccountsRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.BankAccountCreateArgs) {
+    return this.prismaService.bankAccount.create(createDto);
+  }
 }
