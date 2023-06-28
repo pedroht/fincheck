76c206a028c89df8372ed4993be8997ec2ee557f
Pedro Henrique <torres.pedrohen@gmail.com>
2023-06-27 22:58:57 -0300

    feat: implement find all categories by userId service

--- api/src/modules/categories/categories.controller.ts
+++ api/src/modules/categories/categories.controller.ts
@@ -10,19 +10,20 @@ import {
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @Get()
+  findAll(@ActiveUserId() userId: string) {
+    return this.categoriesService.findAllByUserId(userId);
+  }
+
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.categoriesService.findAll();
   }
 
   @Get(':id')
--- api/src/modules/categories/categories.service.ts
+++ api/src/modules/categories/categories.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoriesRepository } from 'src/shared/database/repositories/categories.repositories';
 
 @Injectable()
 export class CategoriesService {
+  constructor(private readonly categoryRepo: CategoriesRepository) {}
+
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  findAllByUserId(userId: string) {
+    return this.categoryRepo.findMany({
+      where: { userId },
+    });
   }
 
   findOne(id: number) {
