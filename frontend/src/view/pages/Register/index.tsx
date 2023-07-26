5dc59d9f4ade37acf02d2270814abe446982af31
Pedro Henrique <torres.pedrohen@gmail.com>
2023-07-26 15:12:32 -0300

    feat: updated register page

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,3 +1,46 @@
+import { Link } from "react-router-dom";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Register() {
-  return <h1>Register Page</h1>
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link to="/login" className="tracking-[-0.5px] text-teal-900 font-medium">
+            Fazer Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          name="name"
+          placeholder="Nome"
+        />
+        <Input
+          name="email"
+          type="email"
+          placeholder="E-mail"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  )
 }
