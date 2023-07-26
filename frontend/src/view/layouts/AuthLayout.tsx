13537228e2364d05dee6e615b76600e48a9f8374
Pedro Henrique <torres.pedrohen@gmail.com>
2023-07-26 13:51:34 -0300

    feat: add AuthLayout

---
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -0,0 +1,34 @@
+import { Outlet } from "react-router-dom";
+
+import { Logo } from "../components/Logo";
+
+import illustration from "../../assets/illustration.png";
+
+export function AuthLayout() {
+  return (
+    <div className="flex w-full h-full">
+      <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-center gap-16">
+        <Logo className="h-6 text-gray-500" />
+
+        <div className="w-full max-w-[504px] px-8">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="hidden w-1/2 h-full lg:flex justify-center items-center p-8 relative">
+        <img
+          src={illustration}
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px] space-y-6">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
