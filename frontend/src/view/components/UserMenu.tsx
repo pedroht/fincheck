da8ea9f39fdbbcde89f8f9a12980d8744afe1993
Pedro Henrique <torres.pedrohen@gmail.com>
2023-08-01 13:17:33 -0300

    feat: update dashboard layout + add header

---
+++ frontend/src/view/components/UserMenu.tsx
@@ -0,0 +1,7 @@
+export function UserMenu() {
+  return (
+    <div className="bg-teal-0 w-12 aspect-square rounded-full flex justify-center items-center border border-teal-100">
+      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">PH</span>
+    </div>
+  )
+}
--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,13 +1,22 @@
-import { useAuth } from "../../../app/hooks/useAuth"
-import { Button } from "../../components/Button";
+import { useAuth } from "../../../app/hooks/useAuth";
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
 
 export function Dashboard() {
   const { signout } = useAuth();
 
   return (
-    <div>
-      <h1>Dashboard page</h1>
-      <Button onClick={signout}>Sair</Button>
-    </div>
+    <div className="h-full w-full p-8 pt-6 flex flex-col gap-4">
+      <header className="h-12 flex items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+
+        <UserMenu />
+      </header>
+
+      <main className="flex-1 flex gap-4">
+        <div className="w-1/2">1</div>
+        <div className="w-1/2">1</div>
+      </main >
+    </div >
   )
 }
