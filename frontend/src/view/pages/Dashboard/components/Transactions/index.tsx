7dec6a19c298c610e1a2de5f295d9db535c854c6
Pedro Henrique <torres.pedrohen@gmail.com>
2023-08-01 17:09:44 -0300

    feat: add Transactions header: transaction filter

--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,7 +1,23 @@
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { FilterIcon } from "../../../../components/icons/FilterIcon";
+import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
+
 export function Transactions() {
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-      <h1>Transactions</h1>
+      <header>
+        <div className="flex items-center justify-between">
+          <button className="flex items-center gap-2">
+            <TransactionsIcon />
+            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
+            <ChevronDownIcon className="text-gray-900" />
+          </button>
+
+          <button>
+            <FilterIcon />
+          </button>
+        </div>
+      </header>
     </div>
   )
 }
