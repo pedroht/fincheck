5ecdd2ddf65e405c1f7ba52cb9b6f8878bea7d42
Pedro Henrique <torres.pedrohen@gmail.com>
2023-08-02 16:51:36 -0300

    feat: add dropdown to usermenu component

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,9 +1,28 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { useAuth } from "../../app/hooks/useAuth";
+import { DropdownMenu } from "./DropdownMenu";
+
 export function UserMenu() {
+  const { signout } = useAuth();
   return (
-    <div className="flex aspect-square w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-0">
-      <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
-        PH
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="flex aspect-square w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-0">
+          <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
+            PH
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          className="flex items-center justify-between gap-2 text-red-800"
+          onSelect={signout}
+        >
+          Sair
+          <ExitIcon className="h-4 w-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
