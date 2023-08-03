fea63c2b38cbab082f104b44141d9203145745cb
Pedro Henrique <torres.pedrohen@gmail.com>
2023-08-03 17:29:52 -0300

    feat: add form to NewAccountModal component

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,5 +1,9 @@
+import { Button } from "../../../../components/Button";
+import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
+import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
+import { Select } from "../../../../components/Select";
 import { useNewAccountModalController } from "./useNewAccountModalController";
 
 export function NewAccountModal() {
@@ -12,7 +16,35 @@ export function NewAccountModal() {
       open={isNewAccountModalOpen}
       onClose={closeNewAccountModal}
     >
-      <InputCurrency />
+      <form>
+        <div>
+          <span className="text-xs tracking-tighter text-gray-600">Saldo</span>
+
+          <div className="flex items-center gap-2">
+            <span className="text-lg tracking-tighter text-gray-600">R$</span>
+            <InputCurrency />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-4">
+          <Input type="text" name="name" placeholder="Nome da Conta" />
+
+          <Select
+            placeholder="Tipo"
+            options={[
+              { label: "Conta Corrente", value: "CHECKING" },
+              { label: "Investimentos", value: "INVESTIMENT" },
+              { label: "Dinheiro Físico", value: "CASH" },
+            ]}
+          />
+
+          <ColorsDropdownInput />
+        </div>
+
+        <Button type="submit" className="mt-6 w-full">
+          Criar
+        </Button>
+      </form>
     </Modal>
   );
 }
